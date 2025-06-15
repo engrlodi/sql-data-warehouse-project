2b48a8ef8e770a53c770b0c0c82b6e7ff90cb388
Dustin Baylon <144006208+engrlodi@users.noreply.github.com>
2025-06-15 21:05:18 +0800

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -17,13 +17,6 @@ Usage Example:
 ===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
-SET @batch_start_time = GETDATE();
-	BEGIN TRY
-
-		SET @batch_start_time = GETDATE();
 		PRINT '========================================';
 		PRINT 'Loading Bronze Layer';
 		PRINT '========================================';
@@ -32,7 +25,7 @@ SET @batch_start_time = GETDATE();
 		PRINT 'Loading CRM Tables';
 		PRINT '----------------------------------------';
 
-	SET @start_time = GETDATE();
+
 		PRINT '>> Truncating Table: bronze.crm_cust_info';
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
@@ -43,11 +36,8 @@ SET @batch_start_time = GETDATE();
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
 
-	SET @start_time = GETDATE();
+
 		PRINT '>> Truncating Table: bronze.crm_prd_info';
 		TRUNCATE TABLE bronze.crm_prd_info;
 		BULK INSERT bronze.crm_prd_info
@@ -57,11 +47,8 @@ SET @batch_start_time = GETDATE();
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
 
-	SET @start_time = GETDATE();
+
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.crm_sales_details;
 		BULK INSERT bronze.crm_sales_details
@@ -72,15 +59,13 @@ SET @batch_start_time = GETDATE();
 		TABLOCK
 
 	);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
+
 
 		PRINT '----------------------------------------';
 		PRINT 'Loading ERP Tables';
 		PRINT '----------------------------------------';
 
-	SET @start_time = GETDATE();
+
 		PRINT '>> Truncating Table: bronze.erp_cust_az12';
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		BULK INSERT bronze.erp_cust_az12
@@ -90,11 +75,8 @@ SET @batch_start_time = GETDATE();
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
 
-	SET @start_time = GETDATE();
+
 		PRINT '>> Truncating Table: bronze.erp_loc_a101';
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		BULK INSERT bronze.erp_loc_a101
@@ -104,11 +86,8 @@ SET @batch_start_time = GETDATE();
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
 
-	SET @start_time = GETDATE();
+
 		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		BULK INSERT bronze.erp_px_cat_g1v2
@@ -118,22 +97,3 @@ SET @batch_start_time = GETDATE();
 		FIELDTERMINATOR = ',',
 		TABLOCK
 );
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
-
-	SET @batch_end_time = GETDATE();
-		PRINT '========================================';
-		PRINT 'Loading Bronze Layer is Completed';
-		PRINT '>> Total Batch Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '========================================';
-
-	END TRY
-	BEGIN CATCH
-		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
-		PRINT 'Error Message' + ERROR_MESSAGE();
-		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
-		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);		
-	END CATCH
-
-END
